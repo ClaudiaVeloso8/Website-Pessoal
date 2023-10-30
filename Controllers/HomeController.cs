642101f64c116eced602445e13f4772785d852ab
ClaudiaVeloso8 <132751861+ClaudiaVeloso8@users.noreply.github.com>
2023-10-30 10:50:20 +0000

    Primeiro commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Website_pessoal.Models;
+
+namespace Website_pessoal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        //referencia acrescentada
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //referencia acrescentada
+        public IActionResult SobreMim()
+        {
+            return View();
+        }
+        //referencia acrescentada
+        public IActionResult Formacao()
+        {
+            return View();
+        }
+        //referencia acrescentada
+        public IActionResult Interesses()
+        {
+            return View();
+        }
+        
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Website_pessoal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
